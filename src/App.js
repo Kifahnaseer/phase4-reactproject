c9c0ad12a3d5da267986ab1407243fdd6967b528
khalid-zein <zeinkhalid004@gmail.com>
2023-04-03 18:06:32 +0300

    finalizing on menu items

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LogIn from './pages/LogIn';
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
-import { useState, useEffect } from 'react';
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    fetch("")
-    .then((res) => {
-      if (res.ok) {
-        res.json()
-        .then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-  // console.log(user)
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={ <LogIn setUser={setUser} /> } />
-          <Route path="/signup" element={ <SignUp setUser={setUser} /> } />
-          <Route path="/" element={ <Home user={user} setUser={setUser} /> } />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return <Layout />;
 }
 
 export default App;
